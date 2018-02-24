c7146339ea08e1a6e50e53d2e6df67e9ac0d827f
viniciusdepaula <viniciusdpaula@gmail.com>
2018-02-24 17:11:56 -0300

    inclusão da validação do EditText

--- DialIntent/app/src/main/java/br/edu/unitri/dialintent/MainActivity.java
+++ DialIntent/app/src/main/java/br/edu/unitri/dialintent/MainActivity.java
@@ -4,9 +4,11 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 /**
  * Exemplo de chamada ao discador padrão do Android
@@ -42,6 +44,15 @@ public class MainActivity extends AppCompatActivity {
     private void callDialer() {
 
         EditText phoneNumber = findViewById(R.id.edtPhoneNumber);
+
+        if(TextUtils.isEmpty(phoneNumber.getText().toString())) {
+
+            Toast.makeText(
+                    getApplicationContext(),
+                    "Informe o número a ser discado!",
+                    Toast.LENGTH_SHORT).show();
+            return;
+        }
 
         Uri number = Uri.parse("tel:" + phoneNumber.getText().toString());
         Intent dialerIntent = new Intent(Intent.ACTION_DIAL, number);
