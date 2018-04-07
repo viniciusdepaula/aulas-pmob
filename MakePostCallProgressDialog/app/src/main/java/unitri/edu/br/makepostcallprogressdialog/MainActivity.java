eccf930da87922df617db68cef392f159f8024aa
viniciusdepaula <viniciusdpaula@gmail.com>
2018-04-07 10:11:32 -0300

    Chamada Volley com progress dialog

---
+++ MakePostCallProgressDialog/app/src/main/java/unitri/edu/br/makepostcallprogressdialog/MainActivity.java
@@ -0,0 +1,137 @@
+package unitri.edu.br.makepostcallprogressdialog;
+
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MainActivity extends AppCompatActivity {
+
+    static String TAG = MainActivity.class.getName();
+
+    ProgressDialog progressDialog;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final Button button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+
+                makePostCall();
+            }
+        });
+    }
+
+    private void makePostCall() {
+        String url = "http://ec2-34-230-46-185.compute-1.amazonaws.com:8080/v1/users/";
+        RequestQueue queue = Volley.newRequestQueue(this);
+
+        /**
+         JsonObjectRequest espera 5 parâmetros
+         Request Type - Tipo da requisição: GET,POST
+         URL          - URL da API
+         JSONObject   - Objeto JSON da requisição (parameters.null se a requisição for do tipo GET)
+         Listener     - Implementação de um Response.Listener() com um callback de sucesso e de erro
+         **/
+
+        JSONObject postRequest = new JSONObject();
+
+        try {
+            postRequest.put("userName", "Manoel");
+            postRequest.put("email", "manoel@gmail.com");
+            postRequest.put("pass", "1234");
+            //postRequest.put("appid","4fa74572c6b3268a6ae5bd1150d7a748");
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        JsonObjectRequest jsonObjReq = new JsonObjectRequest(
+                Request.Method.POST,
+                url,
+                postRequest,
+                new Response.Listener<JSONObject>() {
+
+                    /* Callback chamado em caso de sucesso */
+
+                    @Override
+                    public void onResponse(JSONObject response) {
+
+                        progressDialog.dismiss();
+                        Log.d(TAG, "API Response: " + response);
+                        String message = response.optString("message");
+                        showDialog("Informação", message);
+
+                    }
+                },
+
+                /* Callback chamado em caso de erro */
+
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+
+                        Log.e(TAG, "Ocorreu um erro ao chamar a API " + error);
+                        progressDialog.dismiss();
+                    }
+                }) {
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String, String> params = new HashMap<String, String>();
+                //add params <key,value>
+                return params;
+            }
+
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+
+                Map<String, String> headers = new HashMap<String, String>();
+                String auth = "Basic MzgxNTc5ZmEtZDI0MC00Mzg3LTkyNTMtZWY2YjgwYTdhMWEwOmM4NDM4M2Y0LTJiMDgtNGJiYy04MjQwLWI0YjQ5YTFlYWQzZQ==";
+                headers.put("Authorization", auth);
+                return headers;
+            }
+        };
+
+        queue.add(jsonObjReq);
+        progressDialog = new ProgressDialog(MainActivity.this);
+        progressDialog.setMessage("Por favor, aguarde");
+        progressDialog.show();
+    }
+
+    private void showDialog(String title, String message) {
+        AlertDialog alertDialog = new AlertDialog.Builder(MainActivity.this).create();
+        alertDialog.setTitle(title);
+        alertDialog.setMessage(message);
+        alertDialog.setButton(AlertDialog.BUTTON_POSITIVE, "OK",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                });
+        alertDialog.show();
+    }
+}
